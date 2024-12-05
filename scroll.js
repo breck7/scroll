7b0a9d75990a3bd59c164f7884dc19a3a9f3cf23
breck7 <breck7@gmail.com>
2024-12-05 14:26:48 +0000

    Deploying to wws from @ breck7/scroll@05d009259a0f098d2605da32feca577403f3b7c9 🚀

--- scroll.js
+++ scroll.js
@@ -1,17 +1,15 @@
 #! /usr/bin/env node
 
 // NPM ecosystem includes
-const parseArgs = require("minimist")
 const path = require("path")
-const fs = require("fs")
 const lodash = require("lodash")
-const dayjs = require("dayjs")
 
 // Particles Includes
 const { Particle } = require("scrollsdk/products/Particle.js")
 const { Disk } = require("scrollsdk/products/Disk.node.js")
 const { Utils } = require("scrollsdk/products/Utils.js")
 const { Fusion, FusionFile } = require("scrollsdk/products/Fusion.js")
+const { SimpleCLI } = require("./simpleCli.js")
 const packageJson = require("./package.json")
 
 // Constants
@@ -37,80 +35,6 @@ class ScrollFile extends FusionFile {
   EXTERNALS_PATH = EXTERNALS_PATH
   defaultParserCode = defaultParser.parsersCode
   defaultParser = DefaultScrollParser
-}
-
-const isUserPipingInput = () => {
-  if (process.platform === "win32") return false
-
-  // Check if stdin is explicitly set to a TTY
-  if (process.stdin.isTTY === true) return false
-
-  return fs.fstatSync(0).isFIFO()
-}
-
-class SimpleCLI {
-  CommandFnDecoratorSuffix = "Command"
-
-  executeUsersInstructionsFromShell(args = [], userIsPipingInput = isUserPipingInput()) {
-    const command = args[0] // Note: we don't take any parameters on purpose. Simpler UX.
-    const commandName = `${command}${this.CommandFnDecoratorSuffix}`
-    if (this[commandName]) return userIsPipingInput ? this._runCommandOnPipedStdIn(commandName) : this[commandName](process.cwd())
-    else if (command) this.log(`No command '${command}'. Running help command.`)
-    else this.log(`No command provided. Running help command.`)
-    return this.helpCommand()
-  }
-
-  _runCommandOnPipedStdIn(commandName) {
-    this.log(`Running ${commandName} on piped input`)
-    let pipedData = ""
-    process.stdin.on("readable", function () {
-      pipedData += this.read() // todo: what's the lambda way to do this?
-    })
-    process.stdin.on("end", async () => {
-      const folders = pipedData
-        .trim()
-        .split("\n")
-        .map(line => line.trim())
-        .filter(line => fs.existsSync(line))
-
-      for (const line of folders) {
-        await this[commandName](line)
-      }
-
-      if (folders.length === 0)
-        // Hacky to make sure this at least does something in all environments.
-        // process.stdin.isTTY is not quite accurate for pipe detection
-        this[commandName](process.cwd())
-    })
-  }
-
-  silence() {
-    this.verbose = false
-    return this
-  }
-
-  verbose = true
-
-  log(message) {
-    if (this.verbose) console.log(message)
-    return message
-  }
-
-  get _allCommands() {
-    return Object.getOwnPropertyNames(Object.getPrototypeOf(this))
-      .filter(atom => atom.endsWith(this.CommandFnDecoratorSuffix))
-      .sort()
-  }
-
-  // Normalize 3 possible inputs: 1) cwd of the process 2) provided absolute path 3) cwd of process + provided relative path
-  resolvePath(folder = "") {
-    return path.isAbsolute(folder) ? path.normalize(folder) : path.resolve(path.join(process.cwd(), folder))
-  }
-
-  helpCommand() {
-    this.log(this.welcomeMessage)
-    return this.log(`\nAvailable commands:\n\n${this._allCommands.map(comm => `🖌️ ` + comm.replace(this.CommandFnDecoratorSuffix, "")).join("\n")}\n`)
-  }
 }
 
 class ScrollCli extends SimpleCLI {
@@ -282,6 +206,6 @@ footer.scroll`
   }
 }
 
-if (module && !module.parent) new ScrollCli().executeUsersInstructionsFromShell(parseArgs(process.argv.slice(2))._)
+if (module && !module.parent) new ScrollCli().executeUsersInstructionsFromShell()
 
 module.exports = { ScrollFile, ScrollCli, SimpleCLI, ScrollFileSystem, DefaultScrollParser }
